01c58ba31ee9f9fbe80f1297bcf229de912f4017
Md. Mahbubur Rahman <semahbub@gmail.com>
2020-04-10 21:07:29 +0600

    Initial files are added on April 10, 2020

---
+++ App01_Intro/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using App01_Intro.Models;
+
+namespace App01_Intro.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext (DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<App01_Intro.Models.Person> Person { get; set; }
+    }
+}
---
+++ App01_Intro/Migrations/20200314104727_ScriptA.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App01_Intro.Migrations
+{
+    public partial class ScriptA : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
